bbc1fb63bf33e5c2096dbc908b0cfd3fe9ee31e8
Tewodros Wondimu <tewodros.wr@gmail.com>
2017-10-27 21:11:33 +0200

    Setting up the renderer delegate method

--- DrawAR/ViewController.swift
+++ DrawAR/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+// By inheriting from ARSCNViewDelegate, it allows to get the renderer delete
+class ViewController: UIViewController, ARSCNViewDelegate {
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        
+        // Makes sceneview display performance statistics
+        self.sceneView.showsStatistics = true
+        
+        // Makes self the delegate of the ARSCNViewDelegate
+        self.sceneView.delegate = self
+        
+        self.sceneView.session.run(configuration)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Will be called everytime the scene is rendering
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        print("Rendering")
+    }
 }
 
